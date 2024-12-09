8fff62c9287907535cb685207987d8732b8e7bf5
affggh <879632264@qq.com>
2024-12-09 10:50:42 +0800

    script: Disable simulate mode on dev envionment

--- prebuilt/flash.bat
+++ prebuilt/flash.bat
@@ -14,9 +14,9 @@ if exist "%search_dir%\prog_emmc_firehose_8937_ddr.mbn" (
 
 echo "%search_dir%\rawprogram1.xml"
 if exist "%search_dir%\rawprogram1.xml" (
-    "%local_dir%\xtcfh_loader.exe" --port=\\.\%COMPORT% --memoryname=EMMC --search_path="%search_dir%" --sendxml=rawprogram0.xml,rawprogram1.xml,rawprogram2.xml,patch0.xml --noprompt --reset --simulate  || exit /b 1
+    "%local_dir%\xtcfh_loader.exe" --port=\\.\%COMPORT% --memoryname=EMMC --search_path="%search_dir%" --sendxml=rawprogram0.xml,rawprogram1.xml,rawprogram2.xml,patch0.xml --noprompt --reset || exit /b 1
 ) else (
-    "%local_dir%\xtcfh_loader.exe" --port=\\.\%COMPORT% --memoryname=EMMC --search_path="%search_dir%" --sendxml=rawprogram0.xml,patch0.xml --noprompt --reset --simulate  || exit /b 1
+    "%local_dir%\xtcfh_loader.exe" --port=\\.\%COMPORT% --memoryname=EMMC --search_path="%search_dir%" --sendxml=rawprogram0.xml,patch0.xml --noprompt --reset || exit /b 1
 )
 echo Done!
 echo Your device will reboot!
